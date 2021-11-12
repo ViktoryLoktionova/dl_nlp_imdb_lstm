7d65a2a30328bf1b17188982b96b1afa794672fe
Victor <bulkin_viktor@mail.ru>
2021-11-12 14:15:08 +0300

    Определение тональности текста отзывов на фильмы IMDB с помощью сети LSTM

---
+++ main.py
@@ -0,0 +1,44 @@
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM
+from tensorflow.keras import utils
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import matplotlib.pyplot as plt
+
+max_words = 10000
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_words)
+
+maxlen= 200
+
+x_train = pad_sequences(x_train, maxlen=maxlen)
+x_test = pad_sequences(x_test, maxlen=maxlen)
+
+print(x_train[5002])
+
+model = Sequential()
+model.add(Embedding(max_words, 8, input_length=maxlen))
+model.add(LSTM(32, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics = ['accuracy'])
+
+history = model.fit(x_train,
+                    y_train,
+                    epochs = 15,
+                    batch_size=128,
+                    validation_split=0.1)
+
+plt.plot(history.history['accuracy'],
+         label='Доля верных ответов на обучающем наборе')
+plt.plot(history.history['val_accuracy'],
+         label='Доля верных ответов на проверочном наборе')
+plt.xlabel('Эпоха обучения')
+plt.ylabel('Доля верных ответов')
+plt.legend()
+plt.show()
+
+scores = model.evaluate(x_test, y_test, verbose=1)
+
